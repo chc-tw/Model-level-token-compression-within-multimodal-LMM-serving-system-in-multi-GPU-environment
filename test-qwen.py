e4bcf6e738777df7abd565cbd5381ec0dc017483
Min Lu <mlu316@gatech.edu>
2025-10-20 11:48:57 -0400

    separate vision encoder from text decoder

--- test-qwen.py
+++ test-qwen.py
@@ -1,27 +1,17 @@
-from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor, AutoConfig
 from qwen_vl_utils import process_vision_info
+import torch
+
+model_name = "Qwen/Qwen2.5-VL-3B-Instruct"
+
+# default processer
+processor = AutoProcessor.from_pretrained(model_name)
 
 # default: Load the model on the available device(s)
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-3B-Instruct", torch_dtype="auto", device_map="auto"
+    model_name, torch_dtype="auto", device_map="auto"
 )
-
-# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
-# model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-#     "Qwen/Qwen2.5-VL-3B-Instruct",
-#     torch_dtype=torch.bfloat16,
-#     attn_implementation="flash_attention_2",
-#     device_map="auto",
-# )
-
-# default processer
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
-
-# The default range for the number of visual tokens per image in the model is 4-16384.
-# You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
-# min_pixels = 256*28*28
-# max_pixels = 1280*28*28
-# processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+print(model)
 
 messages = [
     {
@@ -48,15 +38,38 @@ inputs = processor(
     padding=True,
     return_tensors="pt",
 )
-print(inputs)
 inputs = inputs.to("cuda")
 
-# Inference: Generation of the output
-generated_ids = model.generate(**inputs, max_new_tokens=128)
-generated_ids_trimmed = [
-    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)
-print(output_text)
+vision_encoder = model.visual
+test_decoder = model.language_model
+
+output = vision_encoder(inputs['pixel_values'], inputs['image_grid_thw'])
+print(output)
+
+# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+# model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+#     "Qwen/Qwen2.5-VL-3B-Instruct",
+#     torch_dtype=torch.bfloat16,
+#     attn_implementation="flash_attention_2",
+#     device_map="auto",
+# )
+
+
+
+# # The default range for the number of visual tokens per image in the model is 4-16384.
+# # You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
+# # min_pixels = 256*28*28
+# # max_pixels = 1280*28*28
+# # processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+
+
+
+# # Inference: Generation of the output
+# generated_ids = model.generate(**inputs, max_new_tokens=128)
+# generated_ids_trimmed = [
+#     out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+# ]
+# output_text = processor.batch_decode(
+#     generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+# )
+# print(output_text)
