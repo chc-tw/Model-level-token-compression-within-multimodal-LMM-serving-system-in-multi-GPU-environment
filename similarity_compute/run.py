e3d8e8f6f0aebc13d2fc5a8f3e4b372f3c438e92
chc-tw <chc90419@gmail.com>
2025-12-08 03:27:49 +0900

    fix: add similarity computing

--- similarity_compute/run.py
+++ similarity_compute/run.py
@@ -1,21 +1,30 @@
 import evaluate
 import json
+import argparse
 from sentence_transformers import SentenceTransformer
+import optparse
+import os
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-file_path = "/storage/ice1/9/1/cho322/research3/experiments/sharegpt4o_image_caption/dynamic_compression-trace-3-rep-1/Trace with Peak QPS_10.json"
+def main(file_path: str):
+    model = SentenceTransformer("all-MiniLM-L6-v2")
 
-with open(file_path, "r") as f:
-    data = json.load(f)
+    with open(file_path, "r") as f:
+        data = json.load(f)
 
-predictions = [m["generated_text"] for m in data["individual_request_metrics"]]
-references = [m["answer_text"] for m in data["individual_request_metrics"]]
+    predictions = [m["generated_text"] for m in data["individual_request_metrics"]]
+    references = [m["answer_text"] for m in data["individual_request_metrics"]]
 
-# 2. Calculate embeddings by calling model.encode()
-emb_p = model.encode(predictions)
-emb_r = model.encode(references)
+    # 2. Calculate embeddings by calling model.encode()
+    emb_p = model.encode(predictions)
+    emb_r = model.encode(references)
 
-similarities = model.similarity(emb_p, emb_r)
-# compute average similarity on the diagonal
-avg_similarity = similarities.diagonal().mean()
-print(avg_similarity)
+    similarities = model.similarity(emb_p, emb_r)
+    # compute average similarity on the diagonal
+    avg_similarity = similarities.diagonal().mean()
+    print(avg_similarity)
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--file_path", type=str, required=True)
+    args = args.parse_args()
+    main(args.file_path)
